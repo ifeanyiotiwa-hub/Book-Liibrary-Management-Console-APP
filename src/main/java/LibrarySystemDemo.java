a2a84c3ef6cbc356e3048be3f5fe77ee279e9129
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2021-11-03 16:12:50 +0100

    Updated Project

--- src/main/java/LibrarySystemDemo.java
+++ src/main/java/LibrarySystemDemo.java
@@ -43,7 +43,6 @@ public class LibrarySystemDemo {
         System.out.println("========================================================");
 
         printList();
-        System.out.println(priorityQueue.toString());
 
     }
 
