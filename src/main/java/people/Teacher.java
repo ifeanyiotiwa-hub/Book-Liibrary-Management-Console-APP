9fcc195ece89daac725e80a76951481f8aecc4c4
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2021-11-03 08:20:44 +0100

    Updated Teacher Class

---
+++ src/main/java/people/Teacher.java
@@ -0,0 +1,40 @@
+package people;
+
+import libusertype.UserType;
+import model.Book;
+import model.Library;
+import services.BookLender;
+
+import java.util.List;
+
+public class Teacher extends User implements BookLender {
+    public Teacher() {
+        super();
+    }
+
+    public Teacher(String fName, String lName, List<Book> borrowedBookList) {
+        super(fName, lName, borrowedBookList);
+        this.setRole(UserType.TEACHER);
+    }
+
+    @Override
+    public Book returnBook(Book book) {
+        if (getBorrowedBookList().contains(book)){
+            getBorrowedBookList().remove(book);
+            Library.Librarian.getBookShelf().add(book);
+            return book;
+        }
+        else{
+            throw new IllegalStateException("No Such Book in user's book List");
+        }
+    }
+
+    @Override
+    public void borrowBook(Book bookName) {
+        if(Library.Librarian.getBookShelf().contains(bookName)){
+            getBorrowedBookList().add(bookName);
+        }else{
+            throw new IllegalStateException("Book no Available");
+        }
+    }
+}
