0de71fb1ab2e7d37c6b1d7e0be1bc629f4ac8476
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2021-11-03 16:06:44 +0100

    Updated Project

--- src/main/java/services/CustomComparator.java
+++ src/main/java/services/CustomComparator.java
@@ -2,9 +2,11 @@ package services;
 
 import people.Person;
 
-public class CustomComparator {
-        public boolean compare(Person p1, Person p2) {
-            return p1.getRole().getQueuePrecedence() > p2.getRole().getQueuePrecedence();
-        }
+import java.util.Comparator;
+
+public class CustomComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getRole().getQueuePrecedence() - p2.getRole().getQueuePrecedence();
     }
 }
